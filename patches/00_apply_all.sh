eaf1aa33d51168e13fb8f6003c03ddabfc2025ce
Kathryl <25665334+Kathryl@users.noreply.github.com>
2026-03-19 16:36:44 +0000

    Enhancing patches for desanitize

--- patches/00_apply_all.sh
+++ patches/00_apply_all.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -eux
 
 patch -d ~/.wine/drive_c/DCS_server -i ~/patches/01_MissionScripting.lua.patch -p0 --forward --batch || true
-python3 ~/patches/02_WebGUI_reverse_proxy_fix.py
+patch -d ~/.wine/drive_c/DCS_server -i ~/patches/02_MissionScripting.lua.patch -p0 --forward --batch || true
+python3 ~/patches/03_WebGUI_reverse_proxy_fix.py
